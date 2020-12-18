03447254fa9ee05a797aed5aaa3482c47b0522c6
Ignacio Saporiti <ignacio.saporiti@codurance.com>
2020-12-18 18:55:12 +0100

    Adds board-token collaboration

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Square from "../square/Square";
+import useTokenProvider from "../../hooks/token/useTokenProvider";
 
 const SQUARE_AMOUNT: number = 9;
 
 export default function Board() {
+  const tokenProvider: () => string = useTokenProvider();
+
   function buildSquares(): JSX.Element[] {
     const squares: JSX.Element[] = [];
 
     for (let i: number = 0; i < SQUARE_AMOUNT; i++) {
-      squares.push(<Square key={i}/>)
+      squares.push(<Square key={i} tokenProvider={tokenProvider}/>)
     }
 
     return squares;
--- src/components/board/board.spec.tsx
+++ src/components/board/board.spec.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import {render} from "@testing-library/react";
 import Board from "./Board";
+import useTokenProvider from "../../hooks/token/useTokenProvider";
+
+jest.mock('../../hooks/token/useTokenProvider');
 
 describe('Board', () => {
   it('should render 9 Squares inside', () => {
@@ -8,5 +11,11 @@ describe('Board', () => {
     const squares: HTMLElement[] = getAllByLabelText('square');
 
     expect(squares).toHaveLength(9);
+  });
+
+  it('obtains a token provider from useTokenProvider', () => {
+    render(<Board/>);
+
+    expect(useTokenProvider).toBeCalledTimes(1);
   });
 });
